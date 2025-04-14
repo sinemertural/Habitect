c73e010807d177dfdd5eaacdc0153f369bde9114
Sinem Ertural <sinemertural1655@gmail.com>
2025-04-14 11:02:32 +0300

    Initial Commit

---
+++ Habitect/HabitectApp.swift
@@ -0,0 +1,17 @@
+//
+//  HabitectApp.swift
+//  Habitect
+//
+//  Created by Sinem Ertural on 14.04.2025.
+//
+
+import SwiftUI
+
+@main
+struct HabitectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ HabitectTests/HabitectTests.swift
@@ -0,0 +1,17 @@
+//
+//  HabitectTests.swift
+//  HabitectTests
+//
+//  Created by Sinem Ertural on 14.04.2025.
+//
+
+import Testing
+@testable import Habitect
+
+struct HabitectTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
