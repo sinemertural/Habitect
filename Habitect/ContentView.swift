24218c53bb0a22923cc7cb6238ccce3287b94399
Sinem Ertural <sinemertural1655@gmail.com>
2025-05-16 23:13:26 +0300

    Contentview üzerind eclean code ilkesine uyarak düzenlemeler yapildi

--- Habitect/ContentView.swift
+++ Habitect/ContentView.swift
@@ -12,7 +12,7 @@ struct ContentView: View {
 
     var body: some View {
         MainTabView()
-            .environmentObject(habitViewModel) // ✅ BU ŞART!
+            .environmentObject(habitViewModel) 
     }
 }
 
